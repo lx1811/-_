71e2872e99fe984c2f8edb9bbf1e6cf9a2babc8f
FSTACKX <599542779@qq.com>
2020-11-16 20:51:24 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/index.less'
 
+Vue.config.productionTip = false
 
-
-createApp(App).use(store).use(router).mount('#app')
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 
 const routes = [
   
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
   routes
 })
 
